aeed64073078f4c6549204e12fa404eecafbb97f
UnknownUser95 <luluklzde@gmail.com>
2024-07-07 01:54:13 +0200

    add typehints wherever possible

--- src/pickpack/anytree_utils.py
+++ src/pickpack/anytree_utils.py
@@ -1,17 +1,16 @@
-''' anytree_utils.py '''
-
-from typing import Optional
+""" anytree_utils.py """
 
 from anytree import Node, RenderTree, find
 
 
-# Add index property to tree nodes
-def add_indices(tree:RenderTree):
+def add_indices(tree: RenderTree) -> None:
+    """Add index property to tree nodes"""
     for index, row in enumerate(tree):
         row.node.index = index
 
-# Count all leaf (childless) nodes under specified node
-def count_leaves(node:Node, acc: int = 0) -> int:
+
+def count_leaves(node: Node, acc: int = 0) -> int:
+    """Count all leaf (childless) nodes under specified node"""
     if node.children:
         for child in node.children:
             acc += count_leaves(child)
@@ -19,32 +18,36 @@ def count_leaves(node:Node, acc: int = 0) -> int:
         acc += 1
     return acc
 
-# Count all nodes in tree under specified node
-def count_nodes(node:Node, acc: int = 0) -> int:
+
+def count_nodes(node: Node, acc: int = 0) -> int:
+    """Count all nodes in tree under specified node"""
     if node.children:
         for child in node.children:
             acc += count_nodes(child)
     return acc + 1
 
-# Find node using self-defined index property
-def find_by_index(n: Node, index: int) -> Optional[Node]:
-        return find(n, filter_= lambda n : n.index == index)
 
-# Create list of node and all of its descendents (INCLUDES NODE ITSELF at list[0])
-def get_descendants(node:Node, leaves: list[Node] = None) -> list[Node]:
+def find_by_index(node: Node, index: int) -> Node | None:
+    """Find node using self-defined index property"""
+    return find(node, filter_=lambda n: n.index == index)
+
+
+def get_descendants(node: Node, leaves: list[Node] = None) -> list[Node]:
+    """Create list of node and all of its descendants (INCLUDES NODE ITSELF at index 0)"""
     if leaves is None:
         leaves = []
     
     leaves.append(node)
-
+    
     if node.children:
         for child in node.children:
             leaves = get_descendants(child, leaves)
-
+    
     return leaves
 
-# Create list of only leaf (childless) nodes under specified node
-def get_leaves_only(node:Node, leaves: list[Node] = None) -> list[Node]:
+
+def get_leaves_only(node: Node, leaves: list[Node] = None) -> list[Node]:
+    """Create list of only leaf (childless) nodes under specified node"""
     if leaves is None:
         leaves = []
     
